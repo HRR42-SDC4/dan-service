b3461852e10d3142df6f69304e73a6a2508f45b2
Dan Xu <danxu@dans-mbp.lan>
2019-12-18 08:42:36 -0800

    Complete CRUD from API

--- server/app.js
+++ server/app.js
@@ -11,12 +11,34 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static('client'));
 
-app.get('/api/restaurants/:restaurantId', cors(), (req, res) => {
+app.get('/api/restaurants/:restaurantId', (req, res) => {
   var id = req.params.restaurantId;
   db.get(id)
     .then(data => res.send(data))
-    .catch(err => console.log(err));
+    .catch(err => console.log('GET Error:', err));
 });
+
+app.post('/api/restaurants', (req, res) => {
+  const record = req.body;
+  db.insert(record)
+    .then(res.status(201).end())
+    .catch(err => console.log('POST Error:', err));
+});
+
+app.put('/api/restaurants/:restaurantId', (req, res) => {
+  const record = req.body;
+  const id = req.params.restaurantId;
+  db.updateRecord(id, record)
+    .then(res.status(201).end())
+    .catch(err => console.log('PUT Error:', err));
+})
+
+app.delete('/api/restaurants/:restaurantId', (req, res) => {
+  const id = req.params.restaurantId;
+  db.deleteRecord(id)
+    .then(res.status(201).end())
+    .catch(err => console.log('DELETE Error:', err));
+})
 
 const port = 3005;
 app.listen(port, () => console.log(`listening on port ${port}`));
--- server/db/index.js
+++ server/db/index.js
@@ -1,19 +1,36 @@
 const mongoose = require('mongoose');
 const Recs = require('./recs.js');
+mongoose.Promise = global.Promise;
 
 mongoose.set('useCreateIndex', true);
 const db = mongoose.connection;
 mongoose.connect('mongodb://localhost/zagat', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const get = (id) => {
-  return new Promise((resolve, reject) => {
-    Recs
-      .find({ id })
-      .exec((err, doc) => {
-        if (err) reject(err);
-        resolve(doc);
-      });
-  });
+  return Recs.find({id})
+    .exec();
 };
 
-module.exports = { db, get };
+const insert = (record) => {
+  let rec = new Recs(record);
+  return rec.save();
+}
+
+const updateRecord = (id, record) => {
+  return  new Promise((resolve, reject) => {
+    Recs.update({id}, record, (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    })
+  })
+}
+
+const deleteRecord = (id) => {
+  return Recs.find({id})
+    .remove()
+    .exec();
+}
+
+
+
+module.exports = { db, get, insert, updateRecord, deleteRecord};
