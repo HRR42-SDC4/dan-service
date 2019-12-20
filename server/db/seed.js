663a11d098cee6247a2f1e23eb94db88b0d74067
Dan Xu <danxu@dans-mbp.lan>
2019-12-20 15:56:22 -0800

    10M data generated with seed.js

--- server/db/seed.js
+++ server/db/seed.js
@@ -2,12 +2,24 @@ const faker = require('faker');
 const Recs = require('./recs.js');
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-const seed = () => {
+const csvWriter = createCsvWriter({
+  path: `recommendations.csv`,
+  header: [
+    {id: 'id', title: 'Id'},
+    {id: 'genre', title: 'Genre'},
+    {id: 'title', title: 'RestaurantTitle'},
+    {id: 'zecs', title: 'recommendations'},
+  ],
+  fieldDelimiter: ';',
+  append: true
+});
+
+const seed = (num) => {
   var data = [];
   var genres = ['American', 'Asian', 'Mexican', 'Indian'];
   var prices = ['$', '$$', '$$$', '$$$$'];
 
-  for (var i = 1; i <= 100; i++) {
+  for (var i = 1; i <= 1000; i++) {
 
     var genre = genres[Math.floor(Math.random() * Math.floor(genres.length))];
     var title1 = faker.company.companyName();
@@ -15,50 +27,45 @@ const seed = () => {
       id: i,
       genre: genre,
       title: title1,
-      recs: []
+      zecs: []
     }
 
-    var k = Math.floor(Math.random() * 10) + 1;
+    var k = Math.floor(Math.random() * 5) + 1;
     for (var j = 0; j < k; j++) {
       var title2 = faker.company.companyName();
       var price = prices[Math.floor(Math.random() * prices.length)];
       var text = faker.lorem.sentence();
-      var rec = {
+      var zec = {
         pics: [],
         title: title2,
         price: price,
         text: text
       };
 
-      var m = Math.floor(Math.random() * (10 - 5 + 1) + 5);
+      var m = Math.floor(Math.random() * 5 + 1);
       for (var l = 0; l < m; l++) {
-        var photoNum = Math.floor(Math.random() * 74) + 1;
-        var picUrl = `https://zagat-restaurant-images.s3-us-west-1.amazonaws.com/photo${photoNum}.jpg`;
-        rec.pics.push(picUrl);
+        var photoNum = Math.floor(Math.random() * 812) + 1;
+        var picUrl = `https://s3-us-west-1.amazonaws.com/hrr42-sdc4.s3.us-west-1.amazonaws.com/image${photoNum}.jpg`;
+        zec.pics.push(picUrl);
       }
-      recommendationPage.recs.push(rec);
+      recommendationPage.zecs.push(zec);
     }
 
-    recommendationPage.recs = JSON.stringify(recommendationPage.recs);
+    recommendationPage.zecs = JSON.stringify(recommendationPage.zecs);
     data.push(recommendationPage);
   }
 
-  const csvWriter = createCsvWriter({
-    path: 'recommendations.csv',
-    header: [
-      {id: 'id', title: 'Id'},
-      {id: 'genre', title: 'Genre'},
-      {id: 'title', title: 'RestaurantTitle'},
-      {id: 'recs', title: 'recommendations'},
-    ]
-  });
-
   csvWriter
     .writeRecords(data)
-    .then(()=> console.log('The CSV file was written successfully'));
-  // Recs.insertMany(data)
-  //   .then(() => console.log('seeding script complete'))
-  //   .catch(err => console.log(err));
+    .then(()=> {
+      console.log('The CSV file was written successfully');
+      if (num + 1 <= 10000){
+        seed(num + 1);
+      } else {
+        console.log('All the CSV files were written successfully');
+      }
+    })
+    .catch(err => console.log('ERROR:', err))
 };
 
-seed();
+seed(1);
