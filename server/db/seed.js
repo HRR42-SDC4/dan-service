862f2d5b0997e201ce8542f5b457296912b31c1d
Dan Xu <danxu@dans-mbp.lan>
2019-12-21 22:38:37 -0800

    seed.js updated

--- server/db/seed.js
+++ server/db/seed.js
@@ -7,58 +7,52 @@ const csvWriter = createCsvWriter({
   header: [
     {id: 'id', title: 'Id'},
     {id: 'genre', title: 'Genre'},
-    {id: 'title', title: 'RestaurantTitle'},
-    {id: 'zecs', title: 'recommendations'},
+    {id: 'name', title: 'Name'},
+    {id: 'price', title: 'Price Range'},
+    {id: 'description', title: 'Description'},
+    {id: 'pics', title: 'Pictures'},
+    {id: 'recs', title: 'Recommendations'},
   ],
   fieldDelimiter: ';',
   append: true
 });
 
 const seed = (num) => {
-  var data = [];
-  var genres = ['American', 'Asian', 'Mexican', 'Indian'];
-  var prices = ['$', '$$', '$$$', '$$$$'];
+  let data = [];
+  const genres = ['American', 'Asian', 'Mexican', 'Indian'];
+  const prices = ['$', '$$', '$$$', '$$$$'];
 
-  for (var i = 1 + (num - 1) * 1000; i <= 1000 + (num - 1) * 1000; i++) {
-
-    var genre = genres[Math.floor(Math.random() * Math.floor(genres.length))];
-    var title1 = faker.company.companyName();
-    var recommendationPage = {
+  for (let i = 1 + (num - 1) * 1000; i <= 1000 + (num - 1) * 1000; i++) {
+    const genre = genres[Math.floor(Math.random() * Math.floor(genres.length))];
+    const name = faker.company.companyName();
+    let recommendationPage = {
       id: i,
       genre: genre,
-      title: title1,
-      zecs: []
+      name: name,
+      price: prices[Math.floor(Math.random() * prices.length)],
+      description: faker.lorem.sentence(),
+      pics: [],
+      recs: []
     }
 
-    var k = Math.floor(Math.random() * 5) + 1;
-    for (var j = 0; j < k; j++) {
-      var title2 = faker.company.companyName();
-      var price = prices[Math.floor(Math.random() * prices.length)];
-      var text = faker.lorem.sentence();
-      var zec = {
-        pics: [],
-        title: title2,
-        price: price,
-        text: text
-      };
-
-      var m = Math.floor(Math.random() * 5 + 1);
-      for (var l = 0; l < m; l++) {
-        var photoNum = Math.floor(Math.random() * 812) + 1;
-        var picUrl = `https://s3-us-west-1.amazonaws.com/hrr42-sdc4.s3.us-west-1.amazonaws.com/image${photoNum}.jpg`;
-        zec.pics.push(picUrl);
-      }
-      recommendationPage.zecs.push(zec);
+    const randomRecs = Math.floor(Math.random() * 3) + 3;
+    for (let j = 0; j < randomRecs; j++) {
+      recommendationPage.recs.push(Math.floor(Math.random() * 10000000) + 1);
     }
 
-    recommendationPage.zecs = JSON.stringify(recommendationPage.zecs);
+    const randomPics = Math.floor(Math.random() * 4) + 3;
+    for (let k = 0; k < randomPics; k++) {
+      const photoNum = Math.floor(Math.random() * 812) + 1;
+      const picUrl = `https://s3-us-west-1.amazonaws.com/hrr42-sdc4.s3.us-west-1.amazonaws.com/image${photoNum}.jpg`;
+      recommendationPage.pics.push(picUrl);
+    }
+
     data.push(recommendationPage);
   }
 
   csvWriter
     .writeRecords(data)
     .then(()=> {
-      //console.log('The CSV file was written successfully');
       if (num + 1 <= 10000){
         seed(num + 1);
       } else {
